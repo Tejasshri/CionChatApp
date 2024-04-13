c9259e9d51579184af5b308082f68a2137db4667
tejasshri <tejasshrishekhar08@gmail.com>
2024-04-13 17:53:11 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,19 +17,7 @@ import "./App.css";
 export default function App() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    (async function () {
-      try {
-        const response = await fetch("http://192.168.1.37:6001/users");
-        if (response.ok) {
-          const data = await response.json();
-          setUsers(data);
-        }
-      } catch (error) {
-        console.log(error.message, "Somthing went wrong");
-      }
-    })();
-  }, []);
+  
 
   return (
     <ReactContext.Provider value={{ users, setUsers }}>
--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -1,33 +1,79 @@
 import { MdSearch } from "react-icons/md";
+import { v4 } from "uuid";
 
 import styles from "./index.module.css";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ReactContext from "../../context/ReactContext";
 
 export default function Sidebar() {
-  const {users} = useContext(ReactContext)
-  
+  const { users, setUsers } = useContext(ReactContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState("");
+
+  useEffect(() => {
+    (async function () {
+      try {
+        setIsLoading(true);
+        const response = await fetch("http://192.168.1.37:6001/users");
+        if (response.ok) {
+          const data = await response.json();
+          setUsers(data);
+        }
+      } catch (error) {
+        console.log(error.message, "Somthing went wrong");
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
   return (
     <aside>
-      <div className={styles.searchBox}>
-        <button>
-          <MdSearch size={24} color="grey" />
+      <div  className={styles.sidebarHeader}>
+        <div className={styles.searchBox}>
+          <button>
+            <MdSearch size={24} color="grey" />
+          </button>
+          <input type="search" />
+        </div>
+        <button className={styles.addNewBtn}>
+          New
         </button>
-        <input type="search" />
       </div>
       <hr className={styles.hrLine} />
       <ul className={styles.userList}>
-        {
-          users.map(each => (
-            <li key={crypto.randomUUID}>
-              {each.name}
+        {isLoading ? (
+          <LoadingView />
+        ) : (
+          users.map((each) => (
+            <li key={v4()}>
+              <img src={each.imageUrl} alt="" />
+              <div>
+                <h1>{each.name}</h1>
+                <p>{each.phonenumber}</p>
+              </div>
             </li>
           ))
-        }
-        <li>
-
-        </li>
+        )}
+        <li></li>
       </ul>
     </aside>
+  );
+}
+
+function LoadingView() {
+  let n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  return (
+    <>
+      {n.map((each) => (
+        <li key={v4()} className={styles.userPlaceholder}>
+          <div className={styles.placeholderImg}></div>
+          <div className={styles.placeholderText}>
+            <div className={styles.placeholderH1}></div>
+            <div className={styles.placeholderP}></div>
+          </div>
+        </li>
+      ))}
+    </>
   );
 }
