736b407f49d6755a702cc741b41c9e3c0b77aad7
sanjukumarkanki <sanjukanki56429@gmail.com>
2024-04-13 10:25:01 +0530

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>Cion Cancer Clinics</h1>
+      <button>Menu</button>
+    </nav>
+  )
+}
+
+export default Navbar;
